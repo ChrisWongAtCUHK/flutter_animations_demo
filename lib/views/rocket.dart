1bab40b8c954714960cfad320e1ed16473a2758e
Chris Wong <wc38.wong@gmail.com>
2023-06-29 13:08:41 +0800

    Text animation

--- lib/views/rocket.dart
+++ lib/views/rocket.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const _speedOfLight = 100;
+
 class RocketScreen extends StatefulWidget {
   const RocketScreen({super.key});
 
@@ -7,7 +9,17 @@ class RocketScreen extends StatefulWidget {
   State<RocketScreen> createState() => _RocketScreenState();
 }
 
-class _RocketScreenState extends State<RocketScreen> {
+class _RocketScreenState extends State<RocketScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController =
+      AnimationController(vsync: this, duration: const Duration(seconds: 5));
+
+  late final Animation _textAnimation = IntTween(begin: 0, end: _speedOfLight)
+      .animate(CurvedAnimation(
+          parent: _animationController,
+          curve: Curves.easeOut,
+          reverseCurve: Curves.easeIn));
+
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
@@ -23,17 +35,33 @@ class _RocketScreenState extends State<RocketScreen> {
               style: theme.textTheme.headlineMedium,
               textAlign: TextAlign.center,
             ),
-            Text(
-              '0',
-              style: theme.textTheme.headlineLarge?.copyWith(
-                fontSize: 70,
-                color: Colors.green,
-              ),
-              textAlign: TextAlign.center,
-            ),
+            AnimatedBuilder(
+                animation: _animationController,
+                builder: (BuildContext context, _) {
+                  return Text(
+                    _textAnimation.value.toString(),
+                    style: theme.textTheme.headlineLarge?.copyWith(
+                      fontSize: 70,
+                      color: Colors.green,
+                    ),
+                    textAlign: TextAlign.center,
+                  );
+                }),
             const Text(
               'm/s',
               textAlign: TextAlign.center,
+            ),
+            TextButton(
+              onPressed: () {
+                _animationController.forward();
+              },
+              child: const Text('Accelerate'),
+            ),
+            TextButton(
+              onPressed: () {
+                _animationController.reverse();
+              },
+              child: const Text('Decelerate'),
             ),
           ],
         ),
